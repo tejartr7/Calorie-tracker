a8c16625f476a35f91246a7a03d1faef931c3b02
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-12-11 10:58:39 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,9 +9,13 @@ const About = () => {
       <div className="fw-bold contact-container d-flex justify-content-center align-items-center vh-75 m-5" style={{ margin: '20px' }}>
         <div style={{ maxWidth: '500px', width: '90%', padding: '20px', border: '2px solid black', borderRadius: '10px' }}>
           <h1 className="m-3 text-center "><u>About Us</u></h1>
-          <p className='text-center'>
-            Welcome to this calorie tracker website, crafted by RTR—a skilled full-stack web developer and freelancer. In the hustle of our daily lives, managing calorie intake is vital for a healthy lifestyle. This platform reflects my passion for web development and commitment to a user-friendly experience. Empowering you with tools for informed nutritional choices is my primary goal. Explore this space where technology meets wellness, and every click brings you closer to a healthier you. If you desire a similar website, feel free to <a href="mailto:codworldrtr7@gmail.com" style={{color:'black'}}>contact me</a>.
-          </p>
+        <p className='text-center'>
+    Welcome to the Calorie Tracker—an innovative website designed to help you monitor and manage your daily calorie intake. Developed by RTR, a proficient web developer and freelancer, this platform is dedicated to promoting a healthy lifestyle through informed nutritional choices.
+
+    As part of the Calorie Tracker organization, this website not only tracks your calories but also provides timely email updates to support your wellness journey. RTR's expertise in web development ensures a seamless and user-friendly experience.
+
+    Explore this space where technology intersects with wellness, and every interaction brings you closer to achieving a healthier you. If you are interested in a similar website for your organization or have any inquiries about the developer, please don't hesitate to <a href="mailto:codworldrtr7@gmail.com" style={{color:'black'}}>contact RTR</a>.
+    </p>
           <p className='text-center' style={{ color: 'red' }}><u>Note:</u>The webiste is not 100% accurate, so if the calorie count is too high don't worry!!</p>
         </div>
       </div>
