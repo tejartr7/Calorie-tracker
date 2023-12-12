c573044a0c3f14144bafa856a7adb74929f389ab
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-12-12 12:20:21 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='text-center' style={{ position: 'fixed', margin: '0px', bottom: 0, width: '100%', background: 'white', borderTop: '1px solid black', zIndex: 1000 }}>
+    <div className='text-center' style={{ position: 'fixed', margin: '10px', bottom: 0, width: '100%', background: 'white', borderTop: '1px solid black', zIndex: 1000 }}>
       <p style={{ margin: '0px', padding: '0px' }}>Designed and Developed by <a href="#" style={{ color: 'black' }}>RTR  </a>
         Want a website for yourself? <a href='mailto:codworldrtr7@gmail.com' style={{ color: 'black' }}>Contact us</a></p>
     </div>
